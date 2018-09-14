18b90efb25cdb38ac5b038488f9896a4d66320c2
JerryCheWei <jerrychang585@gmail.com>
2018-09-14 11:43:29 +0800

    add JourneyVC headerView

---
+++ Journey/JourneyVC/JourneyViewController.swift
@@ -0,0 +1,19 @@
+//
+//  JourneyViewController.swift
+//  Journey
+//
+//  Created by chang-che-wei on 2018/9/14.
+//  Copyright © 2018年 chang-che-wei. All rights reserved.
+//
+
+import UIKit
+
+class JourneyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
